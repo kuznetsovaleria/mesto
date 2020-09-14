fc85d50a1ecfebd7803da94839dd5c4800f1cb88
Valeria Kuznetsova <kuznetsovaleriaa@yandex.ru>
2020-09-14 21:06:41 +0200

    feat: add second popup

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,6 @@
-const popup = document.querySelector('.popup');
-const popupOpenButton = document.querySelector('.profile__edit-button');
-const popupCloseButton = document.querySelector('.popup__close');
+const editPopup = document.querySelector('.popup');
+const editPopupOpenButton = document.querySelector('.profile__edit-button');
+const editPopupCloseButton = document.querySelector('.popup__close');
 
 const submitForm = document.querySelector('.popup__form');
 const nameInput = document.querySelector('.popup__input_name');
@@ -8,23 +8,37 @@ const professionInput = document.querySelector('.popup__input_profession');
 const nameProfile = document.querySelector('.profile__name');
 const professionProfile = document.querySelector('.profile__profession');
 
-function openPopup () {
-    nameInput.value = nameProfile.textContent;
-    professionInput.value = professionProfile.textContent;
+const addPopup = document.querySelector('.popup_add');
+const addPopupOpenButton = document.querySelector('.profile__add-button');
+const addPopupCloseButton = addPopup.querySelector('.popup__close');
+
+function openPopup (popup) {
     popup.classList.add('popup_opened');
 }
 
-function closePopup () {
+function closePopup (popup) {
     popup.classList.remove('popup_opened');
 }
+
+function copyInput () {
+    nameInput.value = nameProfile.textContent;
+    professionInput.value = professionProfile.textContent;
+}
+
 
 function formSubmitHandler (evt) {
     evt.preventDefault();
     nameProfile.textContent = nameInput.value;
     professionProfile.textContent = professionInput.value;
-    closePopup();
+    closePopup(editPopup);
 }
 
-popupOpenButton.addEventListener('click', openPopup);
-popupCloseButton.addEventListener('click', closePopup);
+editPopupOpenButton.addEventListener('click', () => {copyInput(), openPopup(editPopup)});
+addPopupOpenButton.addEventListener('click', () => {openPopup(addPopup)});
+
+editPopupCloseButton.addEventListener('click', () => {closePopup(editPopup)});
+addPopupCloseButton.addEventListener('click', () => {closePopup(addPopup)});
+
 submitForm.addEventListener('submit', formSubmitHandler);
+
+
