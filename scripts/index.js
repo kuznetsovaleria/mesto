96843fe96ccb92a42e17926fc08d78979bbd9231
Valeria Kuznetsova <kuznetsovaleriaa@yandex.ru>
2020-09-15 20:30:20 +0200

    faet: add template cards

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,4 @@
-const editPopup = document.querySelector('.popup');
+const editPopup = document.querySelector('.popup_edit');
 const editPopupOpenButton = document.querySelector('.profile__edit-button');
 const editPopupCloseButton = document.querySelector('.popup__close');
 
@@ -11,6 +11,78 @@ const professionProfile = document.querySelector('.profile__profession');
 const addPopup = document.querySelector('.popup_add');
 const addPopupOpenButton = document.querySelector('.profile__add-button');
 const addPopupCloseButton = addPopup.querySelector('.popup__close');
+
+const initialCards = [
+    {
+        name: 'Архыз',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+        name: 'Челябинская область',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+        name: 'Иваново',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+        name: 'Камчатка',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+        name: 'Холмогорский район',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+        name: 'Байкал',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+];
+
+const cardTemplate = document.querySelector('.card-template').content;
+const cards = document.querySelector('.cards');
+// const newCard = cardTemplate.cloneNode(true);
+
+function render () {
+    initialCards.forEach(renderCard);
+}
+
+function renderCard({name, link}) {
+    const newCard = cardTemplate.cloneNode(true);
+    
+    newCard.querySelector('.card__place').innerText = name;
+    newCard.querySelector('.card__photo').alt = name;
+    newCard.querySelector('.card__photo').src = link;
+    cards.appendChild(newCard);
+
+}
+
+
+
+function renderCard({name, link}) {
+    const newCard = cardTemplate.cloneNode(true);
+    
+    newCard.querySelector('.card__place').innerText = name;
+    newCard.querySelector('.card__photo').alt = name;
+    newCard.querySelector('.card__photo').src = link;
+    cards.appendChild(newCard);
+    return(newCard);
+
+}
+
+render();
+
+// function addNewCard() {
+//     cards.appendChild(newCard);
+// }
+
+// function render () {
+//     initialCards.forEach(renderCard);
+// }
+
+// render();
+// addNewCard();
+
 
 function openPopup (popup) {
     popup.classList.add('popup_opened');
